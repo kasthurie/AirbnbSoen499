bd442146ff23c406ffb18dd45c3bb7ffda4dce34
niloofarkaypour <50492637+niloofarkaypour@users.noreply.github.com>
2020-03-26 23:45:45 -0400

    Update linearReg.py

--- Airbnb/linearReg.py
+++ Airbnb/linearReg.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-import seaborn as sb
 
 
 df = pd.read_csv('../data/AB_NYC_2019.csv')
