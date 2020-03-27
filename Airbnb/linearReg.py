966aa35f7abc025b160dd18b1f7a31c57635c4ca
niloofarkaypour <50492637+niloofarkaypour@users.noreply.github.com>
2020-03-27 19:09:54 -0400

    Update linearReg.py

--- Airbnb/linearReg.py
+++ Airbnb/linearReg.py
@@ -15,10 +15,25 @@ data = pd.read_csv('../data/AB_NYC_2019.csv')
 data.drop(['last_review', 'id', 'name', 'host_id', 'host_name'], axis=1, inplace=True)
 data.fillna({'reviews_per_month': 0}, inplace=True)
 
-# model price distribution on whole data set
+# Exploratory Data Analysis
 sb.distplot(data['price'], rug=True)
+data.boxplot(column='price', by='neighbourhood_group')
+data.boxplot(column='price', by='neighbourhood')
+data.boxplot(column='price', by='room_type')
+data.boxplot(column='price', by='reviews_per_month')
+plt.figure(figsize=(20, 8))
+plt.scatter(np.log(1+data['reviews_per_month']), data['price'])
+plt.title('Price vs log(reviews_per_month)')
+data.plot.scatter(x='longitude', y='latitude', c='price', cmap='cool', alpha=0.5)
+data[data['price'] < data['price'].mean()].plot.scatter(x='longitude', y='latitude', c='price', cmap='cool', alpha=0.5)
+data[data['price'] > data['price'].mean()].plot.scatter(x='longitude', y='latitude', c='price', cmap='cool', alpha=0.5)
+plt.show()
 
-print(data['room_type'].value_counts())
+print(data.isnull().sum())
+# print(data['price'].value_counts())
+
+
+
 
 
 
