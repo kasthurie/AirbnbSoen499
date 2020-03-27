f58599271aeea9947cb1672600f7fe278268d276
niloofarkaypour <50492637+niloofarkaypour@users.noreply.github.com>
2020-03-26 23:29:08 -0400

    Create linearReg.py

---
+++ Airbnb/linearReg.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import seaborn as sb
+
+
+df = pd.read_csv('../data/AB_NYC_2019.csv')
+df.drop(['last_review'], axis=1, inplace=True)
+df.drop(['id'], axis=1, inplace=True)
+df.drop(['name'], axis=1, inplace=True)
+df.drop(['host_id'], axis=1, inplace=True)
+df.drop(['host_name'], axis=1, inplace=True)
+df.fillna({'reviews_per_month': 0}, inplace=True)
+
+print(df.isnull().sum())
+print(df.shape)
+print(df)
+
+
+
+
+
