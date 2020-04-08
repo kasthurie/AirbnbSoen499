900fd4a8c44e36534828651691d024ff2f007cde
niloofarkaypour <50492637+niloofarkaypour@users.noreply.github.com>
2020-04-08 13:16:32 -0400

    Update linearReg.py

--- Airbnb/linearReg.py
+++ Airbnb/linearReg.py
@@ -39,21 +39,33 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_
 
 model = LinearRegression()
 model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
+y_pred_test = model.predict(x_test)
+y_pred_train = model.predict(x_train)
 
 # Evaluated Metrics
 # MAE(Mean Absolute Error) shows the difference between predictions and actual values
-MAE = mean_absolute_error(y_test, y_pred)
-print('MAE(Mean Absolute Error) = ', MAE)
+MAE_test = mean_absolute_error(y_test, y_pred_test)
+print('MAE(Mean Absolute Error)_test = ', MAE_test)
+MAE_train = mean_absolute_error(y_train, y_pred_train)
+print('MAE(Mean Absolute Error)_train = ', MAE_train)
+
 # MSE (Mean Squared Error)
-MSE = mean_squared_error(y_test, y_pred)
-print('MSE(mean squared error) = ', MSE)
+MSE_test = mean_squared_error(y_test, y_pred_test)
+print('MSE(mean squared error)_test = ', MSE_test)
+MSE_train = mean_squared_error(y_train, y_pred_train)
+print('MSE(mean squared error)_train = ', MSE_train)
+
 # RMSE(Root mean squared error) shows how accurately the model predicts the response
-RMSE = np.sqrt(mean_squared_error(y_test, y_pred))
-print('RMSE(Root mean squared error) = ', RMSE)
+RMSE_test = np.sqrt(mean_squared_error(y_test, y_pred_test))
+print('RMSE(Root mean squared error)_test = ', RMSE_test)
+RMSE_train = np.sqrt(mean_squared_error(y_train, y_pred_train))
+print('RMSE(Root mean squared error)_train = ', RMSE_train)
+
 # R^2(R2 score) will be calculated to find the goodness of fit measure
-accuracy = r2_score(y_test, y_pred)
-print('accuracy(R2 score) = ', accuracy)
+accuracy_test = r2_score(y_test, y_pred_test)
+print('accuracy(R2 score)_test = ', accuracy_test)
+accuracy_train = r2_score(y_train, y_pred_train)
+print('accuracy(R2 score)_train = ', accuracy_train)
 
 # Evaluated predictions
 plt.figure(figsize=(15, 7))
@@ -63,3 +75,4 @@ plt.title('Evaluated predictions', fontsize=15)
 plt.xlabel('Predictions')
 plt.ylabel('Test')
 plt.show()
+#
